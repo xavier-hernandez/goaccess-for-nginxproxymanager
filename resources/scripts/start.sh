2c321aa360f63667c382f2ce021ad48d3af0bb09
Xavier <xavier@xavier.cc>
2022-03-06 14:35:35 -0600

    switched to alpine image
    alpine:3.15 shout out to @martadinata666 for the help

--- resources/scripts/start.sh
+++ resources/scripts/start.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-/usr/bin/tini -s -- nginx
 
-#load archived logs
+#BEGIN - Load archived logs
 if [[ "${SKIP_ARCHIVED_LOGS}" == "True" ]]
 then
     echo "Skipping archived logs as requested..."
     touch /goaccess/access_archive.log
 else
-    echo "Loading archived logs..."
+    count=`ls -1 /opt/log/proxy-host-*_access.log*.gz 2>/dev/null | wc -l`
+    if [ $count != 0 ]
+    then 
+        echo "Loading (${count}) archived logs..."
 zcat -f /opt/log/proxy-host-*_access.log*.gz > /goaccess/access_archive.log
+    else
+        echo "No archived logs found..."
+        touch /goaccess/access_archive.log
 fi
+fi
+#END - Load archived logs
 
-#find active logs
+#BEGIN - Find active logs and check for read access
 proxy_host=""
 
 echo "Checking active logs..."
@@ -22,7 +29,12 @@ do
     then
         if [ -r $file ] && R="Read = yes" || R="Read = No"
         then
-            proxy_host+=" $file"
+            if [ -z "$proxy_host" ]
+            then
+                proxy_host="${proxy_host}${file}"
+            else
+                proxy_host="${proxy_host} ${file}"
+            fi
             echo "Filename: $file | $R"
         else
             echo "Filename: $file | $R"
@@ -40,7 +52,10 @@ then
 else
     echo "Loading proxy-host logs..."    
 fi
+#END - Find active logs and check for read access
 
+#RUN NGINX
+tini -s -- nginx
 
-
-/usr/bin/tini -s -- /goaccess/goaccess /goaccess/access_archive.log ${proxy_host} --no-global-config --config-file=/goaccess-config/goaccess.conf
+#RUN GOACCESS
+tini -s -- /goaccess/goaccess /goaccess/access_archive.log ${proxy_host} --no-global-config --config-file=/goaccess-config/goaccess.conf
