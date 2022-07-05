95c6f984f464dfbed965f620db3ab5d40e2ea284
Xavier Hernandez <xavier@xavier.cc>
2022-07-05 10:30:05 -0500

    merge error

--- resources/scripts/funcs/environment.sh
+++ resources/scripts/funcs/environment.sh
@@ -69,69 +69,7 @@ function set_geoip_database() {
     echo "-------------------------------"
     echo "DEFAULT"
     
-<<<<<<< HEAD
-    echo "<!doctype html><html><head>" > ${4}
-    echo "<title>GOAN - ${goan_version}</title>" >> ${4}
-    echo "<meta http-equiv=\"refresh\" content=\"1\" >" >> ${4}
-    echo "<style>body {font-family: Arial, sans-serif;}</style>" >> ${4}
-    echo "</head><body><p><b>${goan_version}</b><br/><br/>loading... <br/><br/>" >> ${4}
-    echo "Logs processing: $(($goan_proxy_log_count + $goan_proxy_archive_log_count)) (might take some time depending on the number of files to parse)" >> ${4}
-    echo "<br/></p></body></html>" >> ${4}
-}
-
-function load_traefik_logs() {
-    echo -e "\nLOADING TRAEFIK LOGS"
-    echo "-------------------------------"
-
-    goan_proxy_log_count=0
-
-    echo -e "\n#GOAN_PROXY_FILES" >> ${2}
-    if [[ -d "${1}" && -x "${1}" ]];
-    then
-        IFS=$'\n'
-        for file in $(find "${1}" -name 'access.log');
-        do
-            if [ -f $file ]
-            then
-                if [ -r $file ] && R="Read = yes" || R="Read = No"
-                then
-                    echo "log-file ${file}" >> ${2}
-                    goan_proxy_log_count=$((goan_proxy_log_count+1))
-                    echo "Filename: $file | $R"
-                else
-                    echo "Filename: $file | $R"
-                fi
-            else
-                echo "Filename: $file | Not a file"
-            fi
-        done
-        unset IFS
-    else
-        echo "Problem loading directory (check directory or permissions)... ${1}"
-    fi
-
-    if [ $goan_proxy_log_count != 0 ]
-    then
-        echo "Found (${goan_proxy_log_count}) proxy logs..."
-    else
-        echo "No access.log found. Creating an empty log file..."
-        touch "${goan_log_path}/access.log"
-    fi
-
-    echo -e "\nSKIP ARCHIVED LOGS"
-    echo "-------------------------------"
-    echo "FEATURE NOT AVAILABLE FOR TRAEFIK"
-    
-    echo "<!doctype html><html><head>" > ${3}
-    echo "<title>GOAN - ${goan_version}</title>" >> ${3}
-    echo "<meta http-equiv=\"refresh\" content=\"1\" >" >> ${3}
-    echo "<style>body {font-family: Arial, sans-serif;}</style>" >> ${3}
-    echo "</head><body><p><b>${goan_version}</b><br/><br/>loading... <br/><br/>" >> ${3}
-    echo "Logs processing: $(($goan_proxy_log_count)) (might take some time depending on the number of files to parse)" >> ${3}
-    echo "<br/></p></body></html>" >> ${3}
-=======
     echo $'\n' >> ${1}
     echo "#GOAN_MAXMIND_DB" >> ${1}
     echo "geoip-database /goaccess-config/GeoLite2-City.mmdb" >> ${1}
->>>>>>> develop
 }
