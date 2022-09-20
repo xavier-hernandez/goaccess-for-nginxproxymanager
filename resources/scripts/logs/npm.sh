f4a0142ce8778543b4de3431b46ab609db6f2521
Xavier Hernandez <6011223+xavier-hernandez@users.noreply.github.com>
2022-09-20 17:34:39 -0500

    v1.1.7
    - updated maxmind database 9/20/2022
    - removed traversing directory check
    - updated to goaccess 1.6.3
    - added goaccess zip file in the repo, rather than downloading it on build

--- resources/scripts/logs/npm.sh
+++ resources/scripts/logs/npm.sh
@@ -56,7 +56,7 @@ function npm(){
     goan_archive_log_count=0
 
     echo -e "\n#GOAN_NPM_PROXY_FILES" >> ${goan_config}
-    if [[ -d "${goan_log_path}" && -x "${goan_log_path}" ]]; then
+    if [[ -d "${goan_log_path}" ]]; then
         
         echo -e "\n\tAdding proxy logs..."
         IFS=$'\n'
