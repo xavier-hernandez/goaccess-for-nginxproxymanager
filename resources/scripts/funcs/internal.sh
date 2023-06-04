e704bbbd95713d66356697ba308dff4ecc71afd9
Xavier Hernandez <6011223+xavier-hernandez@users.noreply.github.com>
2023-06-03 19:28:58 -0500

    removed readable check

--- resources/scripts/funcs/internal.sh
+++ resources/scripts/funcs/internal.sh
@@ -4,16 +4,10 @@ function checkFile(){
     if [ -z "$1" ]; then
         return 1
     else
-        if [ -e "$1" ] && [ -r "$1" ]; then
-            echo "\tDebug1=$1"
-            echo -e "\tFile $1 exists and is readable"
+        if [ -e "$1" ]; then
+            echo -e "\tFile $1 exists"
             return 0
-        elif [ -e "$1" ]; then
-            echo "\tDebug2=$1"
-            echo -e "\tFile $1 exists but is not readable"
-            return 1
         else
-            echo "\tDebug3=$1"
             echo -e "\tFile $1 does not exist"
             return 1
         fi
