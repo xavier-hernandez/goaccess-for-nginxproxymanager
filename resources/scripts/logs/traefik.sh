ee8ce9e117d1006dec28d6f2bed09a99b3402842
Xavier <xavier@xavier.cc>
2023-08-24 10:07:50 -0500

    v1.1.20
    - added processing the log even if there is an error
    - changed version number

--- resources/scripts/logs/traefik.sh
+++ resources/scripts/logs/traefik.sh
@@ -114,6 +114,6 @@ function traefik(){
     if [[ "${DEBUG}" == "True" ]]; then
         /goaccess-debug/goaccess --debug-file=${goaccess_debug_file} --invalid-requests=${goaccess_invalid_file} --no-global-config --config-file=${goan_config} &
     else
-        /goaccess/goaccess --no-global-config --config-file=${goan_config} &
+        /goaccess/goaccess --num-tests=0 --no-global-config --config-file=${goan_config} &
     fi
 }
