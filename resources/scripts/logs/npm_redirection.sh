3a4ecebc6da9c38944f9d4755b6ab6f8eb090e54
pucherot <pucherot@users.noreply.github.com>
2023-10-15 09:01:49 +0200

    Update npm_redirection.sh

--- resources/scripts/logs/npm_redirection.sh
+++ resources/scripts/logs/npm_redirection.sh
@@ -129,8 +129,10 @@ function npm_redirect(){
 
     echo -e "\nRUN NPM REDIRECT/FALLBACK GOACCESS"
     if [[ "${DEBUG}" == "True" ]]; then
-        /goaccess-debug/goaccess --debug-file=${goaccess_debug_file} --invalid-requests=${goaccess_invalid_file} --no-global-config --config-file=${goan_config} &
+        /goaccess-debug/goaccess --debug-file=${goaccess_debug_file} --invalid-requests=${goaccess_invalid_file} --no-global-config --config-file=${goan_config} \
+          --html-refresh="${HTML_REFRESH}" --keep-last="${KEEP_LAST}" "${GOACCESS_OTHER_OPTIONS" &
     else
-        /goaccess/goaccess --no-global-config --config-file=${goan_config} &
+        /goaccess/goaccess --no-global-config --config-file=${goan_config} \
+          --html-refresh="${HTML_REFRESH}" --keep-last="${KEEP_LAST}" "${GOACCESS_OTHER_OPTIONS" &
     fi
 }
