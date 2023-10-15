6f3dfa5f4aebaa4529e075aad661018234bd2560
pucherot <pucherot@users.noreply.github.com>
2023-10-15 09:02:10 +0200

    Update traefik.sh

--- resources/scripts/logs/traefik.sh
+++ resources/scripts/logs/traefik.sh
@@ -124,8 +124,10 @@ function traefik(){
 
     echo -e "\nRUN TRAEFIK GOACCESS"
     if [[ "${DEBUG}" == "True" ]]; then
-        /goaccess-debug/goaccess --debug-file=${goaccess_debug_file} --invalid-requests=${goaccess_invalid_file} --no-global-config --config-file=${goan_config} &
+        /goaccess-debug/goaccess --debug-file=${goaccess_debug_file} --invalid-requests=${goaccess_invalid_file} --no-global-config --config-file=${goan_config} \
+          --html-refresh="${HTML_REFRESH}" --keep-last="${KEEP_LAST}" "${GOACCESS_OTHER_OPTIONS" &
     else
-        /goaccess/goaccess --num-tests=0 --no-global-config --config-file=${goan_config} &
+        /goaccess/goaccess --num-tests=0 --no-global-config --config-file=${goan_config} \
+          --html-refresh="${HTML_REFRESH}" --keep-last="${KEEP_LAST}" "${GOACCESS_OTHER_OPTIONS" &
     fi
 }
